f9da9e0323b80447f42e02b4d8fe33240c4508d7
Ayush <your_actual_email@gmail.com>
2026-06-11 01:00:05 +0530

    Removed loaddata to fix deployment

--- start.sh
+++ start.sh
@@ -4,12 +4,5 @@ set -e
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Loading initial data..."
-if [ -f "local_data.json" ]; then
-  python manage.py loaddata local_data.json --ignorenonexistent
-else
-  echo "No local_data.json found, skipping data import"
-fi
-
 echo "Starting gunicorn..."
 gunicorn hub_site.wsgi:application
