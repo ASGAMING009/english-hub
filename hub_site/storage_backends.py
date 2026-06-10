ad7ab346ea495a455f91a6aacbcbc09d79d205c5
Ayush <your_actual_email@gmail.com>
2026-06-11 02:39:29 +0530

    Fix CloudinaryMediaStorage _open method

--- hub_site/storage_backends.py
+++ hub_site/storage_backends.py
@@ -1,8 +1,9 @@
+import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from django.core.files.storage import Storage
-from django.conf import settings
-import cloudinary
+from django.core.files.base import ContentFile
+import urllib.request
 
 
 class CloudinaryMediaStorage(Storage):
@@ -13,6 +14,11 @@ class CloudinaryMediaStorage(Storage):
             resource_type='auto'
         )
         return result['public_id']
+
+    def _open(self, name, mode='rb'):
+        url = self.url(name)
+        response = urllib.request.urlopen(url)
+        return ContentFile(response.read())
 
     def url(self, name):
         return cloudinary.CloudinaryImage(name).build_url()
