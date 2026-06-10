4a2de541d290821299127977744a6317a2b2c0f3
Ayush <your_actual_email@gmail.com>
2026-06-11 02:28:23 +0530

    Add logging to debug image upload

--- hub_site/settings/production.py
+++ hub_site/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['the-english-hub.onrender.com', 'localhost', '127.0.0.1']
 
@@ -10,6 +10,20 @@ MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STORAGES["staticfiles"]["BACKEND"] = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = BASE_DIR / "static_compiled"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 
 try:
     from .local import *
